353ba847a2ac7cbcc563ea37e3adeccaf269ad31
Brandon Feltz <elloimbrandon@gmail.com>
2022-07-13 17:04:55 -0700

    did navbar, going to continue styling for mobile

--- personal-web-page/src/App.js
+++ personal-web-page/src/App.js
@@ -1,24 +1,39 @@
 import "./App.scss";
+import { Nav, Navbar } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="container flex flex-col justify-content-center border border-dark w-full">
-      <h1 className="flex justify-content-center border border-dark">
-        Hello World!
-      </h1>
-      <div className="container flex border border-dark justify-content-center w-full">
-        <img
-          className="profile-circle-img"
-          src={require("./images/brfeltz.jpg")}
-          alt="brfeltz"
-        />
+    <>
+      <Navbar
+        // className="flex flex-row border border-dark"
+        collapseOnSelect
+        expand="lg"
+        bg="white"
+        variant="light"
+      >
+        <Navbar.Brand href="#home">BF</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="container flex justify-end border border-dark">
+            <Nav.Link href="/">Test1</Nav.Link>
+            <Nav.Link href="/">Test2</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      {/* change height to screen size */}
+      <div className="container justify-center h-full overflow-scroll flex flex-col align-items-center border border-dark w-screen">
+        <div className="container flex flex-col border border-dark align-items-center justify-content-center w-full">
+          <img
+            className="profile-circle-img"
+            src={require("./images/brfeltz.jpg")}
+            alt="brfeltz"
+          />
+          <div className="border border-dark rounded-3xl mt-2 mb-2 p-2">
+            <h4>Developer | Designer | Student</h4>
+          </div>
+        </div>
       </div>
-      <a href="/" class="arrow-container">
-        <div class="arrow"></div>
-        <div class="arrow"></div>
-        <div class="arrow"></div>
-      </a>
-    </div>
+    </>
   );
 }
 
--- personal-web-page/tailwind.config.js
+++ personal-web-page/tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: { min: "320px", max: "480px" },
+      // => @media (min-width: 640px and max-width: 767px) { ... }
+
+      md: { min: "481px", max: "768px" },
+      // => @media (min-width: 768px and max-width: 1023px) { ... }
+
+      lg: { min: "769px", max: "1024px" },
+      // => @media (min-width: 1024px and max-width: 1279px) { ... }
+
+      xl: { min: "1025px", max: "1200px" },
+      // => @media (min-width: 1280px and max-width: 1535px) { ... }
+
+      "2xl": { min: "1300px", max: "1600" },
+      // => @media (min-width: 1536px) { ... }
+      "3xl": { min: "1600px", max: "2000" },
+    },
+
+    // extend: {},
   },
   plugins: [],
 };
